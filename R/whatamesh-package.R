1c8fd6660242f4509a5c180326edc41287477832
boB Rudis <bob@rud.is>
2018-10-15 10:34:29 -0400

    R package repo initialization complete

---
+++ R/whatamesh-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/whatamesh>
+#' - BugReports: <https://gitlab.com/hrbrmstr/whatamesh/issues>
+#' 
+#' @md
+#' @name whatamesh
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
